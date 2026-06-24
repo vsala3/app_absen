992b616d427793b1024ed43ac21548f7d2428d25
Achmad Yani <iy4n2000@gmail.com>
2026-06-25 01:25:05 +0700

    Fix: Relocate kotlin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,42 +5,20 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val rootProjectBuildDir: Directory = project.layout.buildDirectory.get()
+tasks.register<Delete>("clean") {
+    delete(rootProjectBuildDir)
+}
+
+// Logic pemindahan folder build agar cache rapi di Flutter
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
-
-subprojects {
-    configurations.all {
-        resolutionStrategy {
-            force("com.pichillilorenzo.flutter_inappwebview:flutter_inappwebview_android:1.3.0")
-        }
-    }
-}
-
-// ... (Sisa kode atas biarkan tetap sama)
-
-kotlin {
-    compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
-        // Tambahan opsi untuk membabat warning metadata jika ada library bandel
-        freeCompilerArgs.add("-Xskip-metadata-version-check")
-    }
-}
-
-flutter {
-    source = "../.."
 }
